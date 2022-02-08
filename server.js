86c2a5b812e2bf76e3b639cdd1a5f861ab3c790b
tfavro <wetr9902@gmail.com>
2022-02-07 18:49:11 -0600

    added a query for title

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const app = express();
 //route that the front end can request data from
 const { notes } = require('./data/notes');
 
-
+//Filter data
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.title) {
+      filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    return filteredResults;
+  }
 
 //route that the front end can request data from
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    let results = notes;
+    if(req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
 });
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
